e6d7676792b9fbe0019c6f70bf6783c4938b4aee
satyajit maharana <satyajitmoharana03@gmail.com>
2024-04-02 17:34:04 +0530

    f

---
+++ E-Commerse-Website/Models/AdminHistory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerse_Website.Models
+{
+    public class AdminHistory
+    {
+        [Key]
+        public int AH_id { get; set; }
+        public DateTime AH_time { get; set; }
+        public string? AH_title { get; set; }
+        public string? AH_description { get; set; }
+        public bool AH_deleted { get; set; }
+        public int admin_id { get; set; }
+        public Admin? Admin { get; set; }
+
+    }
+}
---
+++ E-Commerse-Website/Services/Implimentation/IAdminHistoryRepo.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerse_Website.Data;
+using E_Commerse_Website.Models;
+using E_Commerse_Website.Services.IRepo;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerse_Website.Services.Implimentation
+{
+    public interface IAdminHistoryRepo : IRepository<AdminHistory>
+    {
+        //Task Update(AdminHistory obj);
+        Task SoftDelete(int id);
+    }
+    public class AdminHistoryRepo : Repository<AdminHistory>, IAdminHistoryRepo
+    {
+        private readonly myContext _context;
+        public AdminHistoryRepo(myContext db) : base(db)
+        {
+            _context = db;
+        }
+
+        //public async Task Update(AdminHistory obj)
+        //{
+        //    var data = await _context.tbl_adminHistory.Where(o => o.AH_id == obj.AH_id).FirstOrDefaultAsync();
+        //    data.AH_title = obj.AH_title;
+        //}
+
+        public async Task SoftDelete(int id)
+        {
+            var data = await _context.tbl_adminHistory.Where(o => o.AH_id == id).FirstOrDefaultAsync();
+            data.AH_deleted = true;
+        }
+    }
+}
